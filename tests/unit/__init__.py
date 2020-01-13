c9ba7dea8816f94736894fdb5e0df2e84cfd6c59
Paul-Joel <paul.joel@ons.gov.uk>
2020-01-13 17:22:50 +0000

    CR-578 Improve mobile validation: fixed unit tests
    Signed-off-by: Paul-Joel <paul.joel@ons.gov.uk>

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -500,9 +500,9 @@ class RHTestCase(AioHTTPTestCase):
         self.get_accessibility_statement_cy = self.app.router['AccessibilityCY:get'].url_for()
         self.get_accessibility_statement_ni = self.app.router['AccessibilityNI:get'].url_for()
 
-        self.postcode_valid = 'EX26GA'
-        self.postcode_invalid = 'ZZ999ZZ'
-        self.postcode_no_results = 'GU345DU'
+        self.postcode_valid = 'EX2 6GA'
+        self.postcode_invalid = 'ZZ99 9ZZ'
+        self.postcode_no_results = 'GU34 5DU'
 
         self.post_requestcode_address_confirmation_data = {
             'request-address-select': "{'uprn': '10023122451', 'address': '1 Gate Reach, Exeter, EX2 6GA'}"
