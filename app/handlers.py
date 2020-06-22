4220e0a79685d95576faaef0c7a9105f170d9756
Paul-Joel <paul.joel@ons.gov.uk>
2020-06-22 12:56:48 +0100

    CR-000 Serco Redirect Initial push for proof of concept
    Signed-off-by: Paul-Joel <paul.joel@ons.gov.uk>

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,6 @@
 import aiohttp_jinja2
 
-from aiohttp.web import RouteTableDef, json_response
+from aiohttp.web import RouteTableDef, json_response, HTTPFound
 from structlog import get_logger
 
 from . import VERSION
@@ -43,3 +43,38 @@ class Accessibility(View):
             'page_title': page_title,
             'locale': locale
         }
+
+
+@static_routes.view(r'/' + View.valid_display_regions + '/start/launch-eq/')
+class LaunchEQ(View):
+    @aiohttp_jinja2.template('start-launch-eq.html')
+    async def get(self, request):
+        self.setup_request(request)
+        display_region = request.match_info['display_region']
+        self.log_entry(request, display_region + '/start/launch-eq')
+        if display_region == 'cy':
+            locale = 'cy'
+            page_title = 'Launch EQ'
+        else:
+            locale = 'en'
+            page_title = 'Launch EQ'
+        return {
+            'display_region': display_region,
+            'page_title': page_title,
+            'locale': locale,
+            'token': request.query['token']
+        }
+
+    @aiohttp_jinja2.template('start-launch-eq.html')
+    async def post(self, request):
+        self.setup_request(request)
+        display_region = request.match_info['display_region']
+        self.log_entry(request, display_region + '/start/launch-eq')
+
+        data = await request.post()
+
+        token = data.get('token')
+
+        logger.info('redirecting to eq', client_ip=request['client_ip'])
+        eq_url = request.app['EQ_URL']
+        raise HTTPFound(f'{eq_url}/session?token={token}')
