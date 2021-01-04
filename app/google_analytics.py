9b54f32b5fd801ff771d959139d5b8683edb58f3
Paul-Joel <paul.joel@ons.gov.uk>
2021-01-04 12:29:13 +0000

    CR-1521 Update GTM tags and envars Complete
    Signed-off-by: Paul-Joel <paul.joel@ons.gov.uk>

--- app/google_analytics.py
+++ app/google_analytics.py
@@ -1,4 +1,2 @@
 async def ga_ua_id_processor(request):
-    return {'gtm_auth': request.app['GTM_AUTH'],
-            'gtm_preview': request.app['GTM_PREVIEW'],
-            'gtm_cookies_win': request.app['GTM_COOKIES_WIN']}
+    return {'gtm_cont_id': request.app['GTM_CONTAINER_ID'], 'gtm_auth': request.app['GTM_AUTH']}
