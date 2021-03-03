31df22ceecf774af4356e9ee9b26391aff70e0de
Paul-Joel <paul.joel@ons.gov.uk>
2021-03-03 10:20:06 +0000

    CR-1763 IPv6 Tweaks to IP extraction to cope with IPv6 values, and update/split of unit tests
    Signed-off-by: Paul-Joel <paul.joel@ons.gov.uk>

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -325,24 +325,42 @@ class TestUtils(RHTestCase):
         self.before_census_day(2021, 3, 21)
         self.before_census_day(2021, 3, 20)
 
-    def test_client_ip(self):
-
-        request = {'client_id': '36be6b97-b4de-4718-8a74-8b27fb03ca8c', 'trace': '105445aa7843bc8bf206b12000100000'}
+    def test_client_ip_valid(self):
         valid_request = {'client_ip': '192.168.0.0, 35.190.0.0, 35.191.10.0'}
         spoofed_request = {'client_ip': '200.30.20.10, 192.168.0.0, 35.190.0.0, 35.191.10.0'}
-        invalid_request = {'client_ip': 'GHT.RGT.ED.WS, 192.168.0.0, 35.190.0.0, 35.191.10.0'}
-        invalid_request.update(request)
-        single_ip_request = {'client_ip': '35.191.10.0'}
-        single_ip_request.update(request)
+        spoofed_request_ipv6 = \
+            {'client_ip': '2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF, 192.168.0.0, 35.190.0.0, 35.191.10.0'}
         expected_valid = '192.168.0.0'
-        expected_empty = ''
         self.assertEqual(View.single_client_ip(valid_request), expected_valid)
         self.assertEqual(View.single_client_ip(spoofed_request), expected_valid)
+        self.assertEqual(View.single_client_ip(spoofed_request_ipv6), expected_valid)
+
+    def test_client_ip_invalid(self):
+        request = {'client_id': '36be6b97-b4de-4718-8a74-8b27fb03ca8c', 'trace': '105445aa7843bc8bf206b12000100000'}
+        invalid_request = {'client_ip': '?GHT.RGT.ED.WS, 192.168.0.0, 35.190.0.0, 35.191.10.0'}
+        invalid_request.update(request)
+        expected_empty = ''
         with self.assertLogs('respondent-home', 'WARN') as cm:
             self.assertEqual(View.single_client_ip(invalid_request), expected_empty)
             self.assertLogEvent(cm, 'clientIP failed validation. Provided IP - ' + invalid_request['client_ip'],
                                 client_id=request['client_id'], trace=request['trace'])
+
+    def test_client_ip_invalid_single(self):
+        request = {'client_id': '36be6b97-b4de-4718-8a74-8b27fb03ca8c', 'trace': '105445aa7843bc8bf206b12000100000'}
+        single_ip_request = {'client_ip': '35.191.10.0'}
+        single_ip_request.update(request)
+        expected_empty = ''
+        with self.assertLogs('respondent-home', 'WARN') as cm:
             self.assertEqual(View.single_client_ip(single_ip_request), expected_empty)
             self.assertLogEvent(cm, 'clientIP failed validation. Provided IP - ' + single_ip_request['client_ip'],
                                 client_id=request['client_id'], trace=request['trace'])
 
+    def test_client_ip_invalid_ipv6(self):
+        request = {'client_id': '36be6b97-b4de-4718-8a74-8b27fb03ca8c', 'trace': '105445aa7843bc8bf206b12000100000'}
+        invalid_request_ipv6 = {'client_ip': '2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF, 35.190.0.0, 35.191.10.0'}
+        invalid_request_ipv6.update(request)
+        expected_empty = ''
+        with self.assertLogs('respondent-home', 'WARN') as cm:
+            self.assertEqual(View.single_client_ip(invalid_request_ipv6), expected_empty)
+            self.assertLogEvent(cm, 'clientIP failed validation. Provided IP - ' + invalid_request_ipv6['client_ip'],
+                                client_id=request['client_id'], trace=request['trace'])
