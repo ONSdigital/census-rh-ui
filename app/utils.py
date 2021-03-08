367014b8080a69272bd1e57c490ed001ed492c20
Paul-Joel <paul.joel@ons.gov.uk>
2021-03-08 10:07:11 +0000

    CR-1763 IPv6 Change to remove IPv6 regex
    Signed-off-by: Paul-Joel <paul.joel@ons.gov.uk>

--- app/utils.py
+++ app/utils.py
@@ -49,9 +49,8 @@ class View:
     def single_client_ip(request):
         if request['client_ip']:
             client_ip = request['client_ip']
-            ip_validation_pattern = re.compile(r'^[0-9a-zA-Z.:,\s]*$')
             single_ip_validation_pattern = re.compile(r'^[0-9.]*$')
-            if ip_validation_pattern.fullmatch(client_ip) and client_ip.count(',') > 1:
+            if client_ip.count(',') > 1:
                 single_ip_value = client_ip.split(', ', -1)[-3]
                 if single_ip_validation_pattern.fullmatch(single_ip_value):
                     single_ip = single_ip_value
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -330,20 +330,12 @@ class TestUtils(RHTestCase):
         spoofed_request = {'client_ip': '200.30.20.10, 192.168.0.0, 35.190.0.0, 35.191.10.0'}
         spoofed_request_ipv6 = \
             {'client_ip': '2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF, 192.168.0.0, 35.190.0.0, 35.191.10.0'}
+        unknown_request = {'client_ip': 'unknown, 192.168.0.0, 35.190.0.0, 35.191.10.0'}
         expected_valid = '192.168.0.0'
         self.assertEqual(View.single_client_ip(valid_request), expected_valid)
         self.assertEqual(View.single_client_ip(spoofed_request), expected_valid)
         self.assertEqual(View.single_client_ip(spoofed_request_ipv6), expected_valid)
-
-    def test_client_ip_invalid(self):
-        request = {'client_id': '36be6b97-b4de-4718-8a74-8b27fb03ca8c', 'trace': '105445aa7843bc8bf206b12000100000'}
-        invalid_request = {'client_ip': '?GHT.RGT.ED.WS, 192.168.0.0, 35.190.0.0, 35.191.10.0'}
-        invalid_request.update(request)
-        expected_empty = ''
-        with self.assertLogs('respondent-home', 'WARN') as cm:
-            self.assertEqual(View.single_client_ip(invalid_request), expected_empty)
-            self.assertLogEvent(cm, 'clientIP failed validation. Provided IP - ' + invalid_request['client_ip'],
-                                client_id=request['client_id'], trace=request['trace'])
+        self.assertEqual(View.single_client_ip(unknown_request), expected_valid)
 
     def test_client_ip_invalid_single(self):
         request = {'client_id': '36be6b97-b4de-4718-8a74-8b27fb03ca8c', 'trace': '105445aa7843bc8bf206b12000100000'}
